bf7b3c3efc046a2f23d371edaf1f537f075a4ba6
bellaleda <134225951+bellaleda@users.noreply.github.com>
2023-08-04 22:11:34 -0300

    estudos js

---
+++ Lógica de Programação/programa.js
@@ -0,0 +1,11 @@
+console.log("=== GAROTA DE PROGRAMA ===")
+
+// Entrada (dados): capturar dados so user
+//janela q permite entrada de dados. quando executado, devove uma resposta, o que foi digitado, return. const para guardar a info
+let nome = prompt("Insira seu nome");
+
+// Processamento - recebe um dado e formata a informação, processa, trabalha 
+nome = nome.toUpperCase(); 
+
+// Saída de dados - resultado
+document.write(nome);
